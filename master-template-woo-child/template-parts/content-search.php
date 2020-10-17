ac33b7068642549f9e6c468ec74d7e5cd5cab783
Angel Burgos <angelpublicista.1@gmail.com>
2020-10-16 20:19:11 -0500

    Ajuste resultados de búsqueda

--- master-template-woo-child/template-parts/content-search.php
+++ master-template-woo-child/template-parts/content-search.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Hola mundo";
+?>
+
+<section class="content-search">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="search-title"><?php echo $wp_query->found_posts; ?> <?php _e( 'Search Results Found For', 'locale' ); ?>: "<?php the_search_query(); ?>"</h1>
+        </div>
+        <div class="col-12 col-md-6">
+            <a href="<?php the_permalink(); ?>" class="card shadow p-4 d-block rounded">
+                <div class="card-body ">
+                    <span class="card-date">Posted on <?php the_date(); ?></span>
+                    <h4 class="card-title mt-2"><?php the_title(); ?></h4>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
